65a8612af816bcc1fc8696d72930faef2f28d3dd
Ray'han Neeamuth <rayhanneeamuth@gmail.com>
2025-09-19 10:13:22 -0400

    Fix

--- Employee.java
+++ Employee.java
@@ -1,74 +1,87 @@
+import lang.stride.*;
+
 /**
  * Consider this incomplete Employee class to answer some questions of your exam
- *
- * @author Tassia
- * @version 0.1
+ * @author Tassia @version 0.1
  */
-public class Employee{
+public class Employee
+{
     private String fullname;
     private int yearOfBirth;
     private boolean onLeave;
     private double hourlyWage;
     private double unpaidHours;
 
-        public Employee(String fullname, int yearOfBirth, double hourlyWage){
+    /**
+     * 
+     */
+    public Employee(String fullname, int yearOfBirth, double hourlyWage)
+    {
         this.fullname = fullname;
         this.yearOfBirth = yearOfBirth;
         this.hourlyWage = hourlyWage;
         onLeave = false;
-        unpaidHours = 0.0;
+        unpaidHours = 11;
     }
-    
+
     /**
-     * Full name 
+     * Setter method fullname
      */
-        public Employee(String fullname){
-        fullname= fullname;
+    public Employee(String fullname)
+    {
+        fullname = fullname;
     }
+
     /**
-     * Full name
+     * Getter method fullname
      */
-        public Employee(){
-        return fullname;
-        }
-    
+    public int getFullname(int fullname)
+    {
+        return (fullname);
+    }
+
     /**
-     * Returns true is an employee is above 16 years old
-     */
-        private int calculateAge(int currentYear){
+     * Returns true is an employee is above 16 years old  
+     */
+    private int calculateAge(int currentYear)
+    {
         return (currentYear - yearOfBirth);
     }
 
     /**
      * Returns true is an employee is above 16 years old
      */
-        public boolean canDrive(){
+    public boolean canDrive()
+    {
         int age = calculateAge(2025);
-        if(age >= 16);
-        printf("can drive");
-        if(age<= 16)
-        printf("uneligeable to drive until 16 years old or above");
-        
+        if (age >= 16) {
+            System.out.println("can drive");
+        }
+        if (age <= 16) {
+            System.out.println("uneligeable to drive until 16 years old or above");
+        }
     }
 
-    /*
-     * Returns the net pay for the outstanding unpaid hours
+    /**
+     * 
      */
-        private double calculatePay(){
-        if(unpaidHours> 0)
-        printf("hours worked x hourly wage");
-        
-        
-        
-    }
+    private double calculatePay()
+    {
+        if (unpaidHours == 11) {
+            System.out.println("Pay + 30% tax + unpaidHours amount");
+        }
+       }
 
-    /*
-     * Output the payment record and resets unpaid hours
+    /* Output the payment record and resets unpaid hours*/
+
+    /**
+     * 
      */
-    public void paySalary(){
-        if(hoursworked= 0)
-        printf("total pay");
-        if(unpaidhours>= 0)
-        printf("Total pay - unpaid hours x hourly wage");
+    public void paySalary()
+    {
+        /* Unsupported feature in Stride : = in expression*/
+        if (unpaidHours == 11) {
+            System.out.println("total pay");
+        }
     }
 }
